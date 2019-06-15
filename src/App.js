0b7e2dfd71e27b8c68182227eb3715f71bf0ceb6
Somtochukwu Medua-Ugeh <smugeh@gmail.com>
2019-06-15 09:18:54 +0100

    Installed dependency and initial setup for theme controls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Home from "./components/Home";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+
+const theme = { mode: "dark" }; // controls for theming
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${props => (props.theme.mode === "dark" ? "#111" : "#EEE")};
+    color: ${props => (props.theme.mode === "dark" ? "#EEE" : "#111")};
+  }
+`;
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <Home />
+      </>
+    </ThemeProvider>
   );
 }
 
