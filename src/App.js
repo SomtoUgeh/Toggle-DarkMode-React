4ad88ae2fd08cbc7f7b150564cd5325c599eeaf2
Somtochukwu Medua-Ugeh <smugeh@gmail.com>
2019-06-15 09:52:41 +0100

    Added localstorage fallback for saving theme options

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./components/Home";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
+import storage from "local-storage-fallback";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -9,8 +10,19 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+function getInitialTheme() {
+  const savedTheme = storage.getItem("theme");
+  if (savedTheme) {
+    return savedTheme ? JSON.parse(savedTheme) : { mode: "light" };
+  }
+}
+
 function App() {
-  const [theme, setTheme] = useState({ mode: "light" });
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  useEffect(() => {
+    storage.setItem("theme", JSON.stringify(theme));
+  }, [theme]);
 
   return (
     <ThemeProvider theme={theme}>
