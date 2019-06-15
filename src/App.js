14fd923ffaefe99445a896d922febcc3cec856da
Somtochukwu Medua-Ugeh <smugeh@gmail.com>
2019-06-15 09:18:54 +0100

    Installed dependency and initial setup for theme controls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./components/Home";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${props => (props.theme.mode === "dark" ? "#111" : "#EEE")};
+    color: ${props => (props.theme.mode === "dark" ? "#EEE" : "#111")};
+  }
+`;
 
 function App() {
+  const [theme, setTheme] = useState({ mode: "light" });
+
   return (
-    <>
-      <Home />
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        <Home />
+
+        <label htmlFor="theme-controller" class="switch">
+          <input
+            type="checkbox"
+            id="theme-controller"
+            onChange={e => setTheme(theme.mode === "dark" ? { mode: "light" } : { mode: "dark" })}
+          />
+          <span class="slider round" />
+        </label>
+      </>
+    </ThemeProvider>
   );
 }
 
